84f418dde84b9270fe7d2b19c8c57d4c86b4109f
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-31 02:12:05 +0300

    initial commit

--- query/index.js
+++ query/index.js
@@ -1,19 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const axios = require('axios')
 
 app.use(express.json())
 app.use(cors())
 
 const posts = {}
 
-
-app.get('/posts', (request, response) => {
-    response.send(posts)
-})
-
-app.post('/events', (request, response) => {
-    const { type, data } = request.body
+const handleEvent = (type, data) => {
 
     if (type === 'PostCreated') {
         const { id, title } = data
@@ -24,12 +19,37 @@ app.post('/events', (request, response) => {
 
         const post = posts[postId]
         post.comments.push({ id, content, status })
-    }
+    } else if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data
 
-    console.log(posts)
+        const post = posts[postId]
+        const comment = post.comments.find(comment => comment.id === id)
+        comment.status = status
+        comment.content = content
+    }
+}
+
+
+app.get('/posts', (request, response) => {
+    response.send(posts)
+})
+
+app.post('/events', (request, response) => {
+    const { type, data } = request.body
+
+
+    handleEvent(type, data)
     response.send({})
 })
 
-app.listen(4002, () => {
+app.listen(4002, async() => {
     console.log('server running on port 4002')
+
+    const res = await axios.get('http://localhost:4005/events')
+
+    for (let event of res.data) {
+        console.log('Processing event', event.type)
+        handleEvent(event.type, event.data)
+    }
+
 })
