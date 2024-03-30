52bda82f33f2ae487dcf39e9b3a73c7edb6d3094
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-30 23:31:40 +0300

    initial commit

---
+++ client/src/components/CommentList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function CommentList({comments}) {
+
+    const renderedComments = comments.map(comment => {
+        return <li key={comment.id}>{comment.content}</li>
+    })
+
+    return (
+        <ul className="">
+            {renderedComments}
+        </ul>
+    )
+}
